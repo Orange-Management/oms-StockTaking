cbea789f88c802d0183bff70bdff0d85ff0f6f54
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-08-17 20:02:29 +0200

    fix #96 fix #97

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -1,12 +1,20 @@
 <?php
 
 use phpOMS\Router\RouteVerb;
+use phpOMS\Account\PermissionType;
+use Modules\StockTaking\Models\PermissionState;
+use Modules\StockTaking\Controller;
 
 return [
     '^.*/backend/warehouse/stocktaking/list.*$' => [
         [
             'dest' => '\Modules\StockTaking\Controller:viewStockTakingList',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::STOCK_TAKING,
+            ],
         ],
     ],
 ];
---
+++ Models/PermissionState.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.2
+ *
+ * @package    Modules\StockTaking
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+declare(strict_types=1);
+
+namespace Modules\StockTaking\Models;
+
+use phpOMS\Stdlib\Base\Enum;
+
+/**
+ * Permision state enum.
+ *
+ * @package    Modules\StockTaking
+ * @license    OMS License 1.0
+ * @link       http://website.orange-management.de
+ * @since      1.0.0
+ */
+abstract class PermissionState extends Enum
+{
+    public const STOCK_TAKING = 1;
+}
