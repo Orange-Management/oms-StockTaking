a2bf2ebba37008fea0f229b9922b68b746d285b0
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-09-19 21:34:04 +0200

    Split controllers per application

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -3,15 +3,15 @@
 use phpOMS\Router\RouteVerb;
 use phpOMS\Account\PermissionType;
 use Modules\StockTaking\Models\PermissionState;
-use Modules\StockTaking\Controller;
+use Modules\StockTaking\Controller\BackendController;
 
 return [
     '^.*/backend/warehouse/stocktaking/list.*$' => [
         [
-            'dest' => '\Modules\StockTaking\Controller:viewStockTakingList',
+            'dest' => '\Modules\StockTaking\Controller\BackendController:viewStockTakingList',
             'verb' => RouteVerb::GET,
             'permission' => [
-                'module' => Controller::MODULE_NAME,
+                'module' => BackendController::MODULE_NAME,
                 'type'  => PermissionType::READ,
                 'state' => PermissionState::STOCK_TAKING,
             ],
