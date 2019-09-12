5642e035dc382cda88d67d653555488ce7e82ed7
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-09-12 19:48:00 +0200

    fix phpcs findings

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -4,11 +4,11 @@
  *
  * PHP Version 7.4
  *
- * @package    Modules\StockTaking
- * @copyright  Dennis Eichhorn
- * @license    OMS License 1.0
- * @version    1.0.0
- * @link       https://orange-management.org
+ * @package   Modules\StockTaking
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
  */
 declare(strict_types=1);
 
@@ -22,22 +22,24 @@ use phpOMS\Views\View;
 /**
  * StockTaking controller class.
  *
- * @package    Modules\StockTaking
- * @license    OMS License 1.0
- * @link       https://orange-management.org
- * @since      1.0.0
+ * @package Modules\StockTaking
+ * @license OMS License 1.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
  */
 final class BackendController extends Controller
 {
 
     /**
+     * Routing end-point for application behaviour.
+     *
      * @param RequestAbstract  $request  Request
      * @param ResponseAbstract $response Response
      * @param mixed            $data     Generic data
      *
      * @return RenderableInterface
      *
-     * @since  1.0.0
+     * @since 1.0.0
      * @codeCoverageIgnore
      */
     public function viewStockTakingList(RequestAbstract $request, ResponseAbstract $response, $data = null) : RenderableInterface
