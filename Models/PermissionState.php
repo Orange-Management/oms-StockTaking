c32206ab2ddf332e04312090fd0839a3123dba3e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-09-28 20:59:03 +0200

    style fixes and time recording app draft

--- Models/PermissionState.php
+++ Models/PermissionState.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 7.4
  *
- * @package   Modules\StockTaking
+ * @package   Modules\StockTaking\Models
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -19,7 +19,7 @@ use phpOMS\Stdlib\Base\Enum;
 /**
  * Permision state enum.
  *
- * @package Modules\StockTaking
+ * @package Modules\StockTaking\Models
  * @license OMS License 1.0
  * @link    https://orange-management.org
  * @since   1.0.0
